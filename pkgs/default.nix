1eca8d70a68a9cb65d2ab2d4ee9465b9d8269cce
Jurien Hamaker <jurien@employes.nl>
2023-01-10 12:49:18 +0100

    chore: remove waynergy since it's part of nixpkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,5 +4,4 @@
   catppuccin-cursors = pkgs.callPackage ./catppuccin-cursors { };
   catppuccin-gtk = pkgs.callPackage ./catppuccin-gtk { };
   beekeeper-studio = pkgs.callPackage ./beekeeper-studio { };
-  waynergy = pkgs.callPackage ./waynergy { };
 }
