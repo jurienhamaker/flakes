e14defc367cc7ac91c20048c8d01fa16bfc0407f
Jurien Hamaker <jurien@employes.nl>
2023-03-01 11:12:25 +0100

    chore: update wallpaper

--- modules/desktop/hyprland/default.nix
+++ modules/desktop/hyprland/default.nix
@@ -3,13 +3,7 @@
 let
   default_wall = pkgs.writeShellScript "default_wall" ''
     killall dynamic_wallpaper
-      if [[ "$GTK_THEME" == "Catppuccin-Frappe-Pink" ]]; then
-        ${pkgs.swww}/bin/swww img "${../../theme/catppuccin-dark/common/wall/default.png}" --transition-type random
-      elif [[ "$GTK_THEME" == "Catppuccin-Latte-Green" ]]; then
-        ${pkgs.swww}/bin/swww img "${../../theme/catppuccin-light/common/wall/default.png}" --transition-type random
-      else 
-        ${pkgs.swww}/bin/swww img "${../../theme/nord/common/wall/default.png}" --transition-type random
-      fi
+    ${pkgs.swww}/bin/swww img "${../../theme/yugen/common/wall/default.png}" --transition-type random
   '';
 in
 
