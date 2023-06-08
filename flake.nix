a63bbe26453796eb05cd724186113bee5e65be0c
Jurien Hamaker <whoami@jurien.dev>
2023-06-08 11:36:03 +0200

    chore: use unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs =
     {
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
       rust-overlay.url = "github:oxalica/rust-overlay";
       impermanence.url = "github:nix-community/impermanence";
       nur.url = "github:nix-community/NUR";
